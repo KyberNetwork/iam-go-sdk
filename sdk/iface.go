e39813a45f779ee5764987daaa2b73d100fae5a7
anhnv <anhvietnguyen.nva@gmail.com>
2023-12-18 11:13:51 +0700

    feat: add func CreatePermissionMultipleObjects

--- sdk/iface.go
+++ sdk/iface.go
@@ -19,9 +19,16 @@ type ISDK interface {
 	CheckEditorPermission(namespace string, object string, subjectID string) (bool, error)
 	CheckOwnerPermission(namespace string, object string, subjectID string) (bool, error)
 	CheckConsumerPermission(namespace string, object string, subjectID string) (bool, error)
+
 	CreatePermission(request *dto.CreatePermissionRequest, bearerAccessToken string) (string, error)
 	CreateViewerPermission(namespace string, object string, subjectID string, bearerAccessToken string) (string, error)
 	CreateEditorPermission(namespace string, object string, subjectID string, bearerAccessToken string) (string, error)
 	CreateOwnerPermission(namespace string, object string, subjectID string, bearerAccessToken string) (string, error)
 	CreateConsumerPermission(namespace string, object string, subjectID string, bearerAccessToken string) (string, error)
+
+	CreatePermissionMultipleObjects(request *dto.CreatePermissionMultipleObjectsRequest, bearerAccessToken string) ([]string, error)
+	CreateViewerPermissionMultipleObjects(namespace string, objects []string, subjectID string, bearerAccessToken string) ([]string, error)
+	CreateEditorPermissionMultipleObjects(namespace string, objects []string, subjectID string, bearerAccessToken string) ([]string, error)
+	CreateOwnerPermissionMultipleObjects(namespace string, objects []string, subjectID string, bearerAccessToken string) ([]string, error)
+	CreateConsumerPermissionMultipleObjects(namespace string, objects []string, subjectID string, bearerAccessToken string) ([]string, error)
 }
